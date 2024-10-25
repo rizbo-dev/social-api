0517f5f14f1a4c5a2c219c7e38fb2e35fee69fc4
rizbo <boris.evetovic777@gmail.com>
2024-10-25 21:55:26 +0200

    Implemented storage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/rizbo-dev/social-api/internal/env"
+	"github.com/rizbo-dev/social-api/internal/store"
 )
 
 func main() {
@@ -17,8 +18,12 @@ func main() {
 	config := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config,
+		store,
 	}
 
 	mux := app.mount()
