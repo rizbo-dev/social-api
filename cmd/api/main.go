c4afe575a74b1a6074c25fce790f94eb3648972b
rizbo <boris.evetovic777@gmail.com>
2024-10-27 12:01:09 +0100

    Added db connection, created docker compose for db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
+	"github.com/rizbo-dev/social-api/internal/db"
 	"github.com/rizbo-dev/social-api/internal/env"
 	"github.com/rizbo-dev/social-api/internal/store"
 )
@@ -17,9 +18,29 @@ func main() {
 
 	config := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:               env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConnections: env.GetInt("DB_MAX_OPEN_CONNECTIONS", 30),
+			maxIdleConnections: env.GetInt("DB_MAX_IDLE_CONNECTIONS", 30),
+			maxIdleTime:        env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		config.db.addr,
+		config.db.maxOpenConnections,
+		config.db.maxIdleConnections,
+		config.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Printf("database connection pool estaplished")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config,
---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+func New(addr string, maxOpenConnections, maxIdleConnections int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConnections)
+	db.SetMaxIdleConns(maxIdleConnections)
+
+	duration, err := time.ParseDuration(maxIdleTime)
+
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
